1baa3b00fee133a4e2da4a06d5fbeaf9af61b847
xidoke <do.pd200154@sis.hust.edu.vn>
2023-11-25 20:19:39 +0700

    add getURLsFromHTMLtest

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const document = dom.window.document;
+    const listTagA = document.querySelectorAll("a");
+    listTagA.forEach(link => {
+        if (link.href.slice(0, 1) === '/') {
+            try {
+                const relURL = new URL(`${baseURL}${link.href}`);
+                urls.push(relURL.href)
+            } catch (error) {
+                console.log('error URL');
+            }
+        } else {
+            try {
+              const relURL = new URL(`${link.href}`);
+              urls.push(relURL.href);
+            } catch (error) {
+              console.log("error URL");
+            }
+        }
+    })
+    return urls
+}
+
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +37,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl')
+const { normalizeURL, getURLsFromHTML } = require('./crawl')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL', () => {
@@ -20,9 +20,53 @@ test('normalizeURL CAPITAL', () => {
     expect(actual).toEqual(expected)
 })
 
-test('normalizeURL trp http', () => {
+test('normalizeURL trip http', () => {
     const input = 'http://facebook.com/path/'
     const actual = normalizeURL(input)
     const expected = 'facebook.com/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML absolutely URL', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://facebook.com/account/">Facebook account</a>
+    </body>
+</html>
+    `
+    const inputBaseURL = 'https://facebook.com'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://facebook.com/account/"]
+    expect(actual).toEqual(expected)
+})
+  
+test('getURLsFromHTML relative URL and absolutely', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/account/">Facebook account</a>
+        <a href="https://facebook.com/login/">Facebook login</a>
+    </body>
+</html>
+    `
+    const inputBaseURL = 'https://facebook.com'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://facebook.com/account/", "https://facebook.com/login/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid"></a>
+    </body>
+</html>
+    `
+    const inputBaseURL = 'https://facebook.com'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
